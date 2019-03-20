aec2c1a4f3e797bb72ff971df453c04ecf77d4cf
Partha Protim Paul <41873352+Partha-SUST16@users.noreply.github.com>
2019-03-20 21:35:03 +0600

    Abstract Factory and Factory Pattern

---
+++ src/Factory/ShapeFactory.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Factory;
+
+/**
+ *
+ * @author USER
+ */
+public class ShapeFactory {
+	
+   //use getShape method to get object of type shape 
+   public Shape getShape(String shapeType){
+      if(shapeType == null){
+         return null;
+      }		
+      if(shapeType.equalsIgnoreCase("CIRCLE")){
+         return new Circle();
+         
+      } else if(shapeType.equalsIgnoreCase("RECTANGLE")){
+         return new Rectangle();
+         
+      } else if(shapeType.equalsIgnoreCase("SQUARE")){
+         return new Square();
+      }
+      
+      return null;
+   }
+}
